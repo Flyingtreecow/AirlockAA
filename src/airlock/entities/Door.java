07d8374b1fb1c835c8deb8bf063f4e0e969bd18c
Flyingtreecow <angrybirdnealon@gmail.com>
2022-11-22 15:45:52 +1100

    Implementing Door + Setting up testing

--- src/airlock/entities/Door.java
+++ src/airlock/entities/Door.java
@@ -10,48 +10,68 @@ public class Door implements IDoor{
 	IPressureSensor exSensor;
 	
 	private DoorState state;
-	
-	public Door(IPressureSensor exSensor, IPressureSensor inSensor, 
-	            DoorState initialState) throws DoorException {
-		//TODO - implement method
+	private Double diffInPressure = inSensor.getPressure()- exSensor.getPressure();
+        
+	public Door(IPressureSensor exSensor, IPressureSensor inSensor, DoorState initialState) throws DoorException {
+            try{
+               exSensor.getPressure();
+               inSensor.getPressure();
+            }
+            catch(Exception e){
+               throw new DoorException("Sensors are not valid");
+            }
+            inSensor = this.inSensor;
+            exSensor = this.exSensor;
+            state = initialState;
+            
+            if(state == DoorState.OPEN &&  (diffInPressure > TOLERANCE || diffInPressure < TOLERANCE)){
+               throw  new DoorException("Difference in pressures is too large");
+            }
 	}
 	
 	@Override
 	public void open() throws DoorException {
-		//TODO - implement method
+            if(state == DoorState.OPEN){
+               throw  new DoorException("Door already open");
+            }
+            if(state == DoorState.CLOSED && (diffInPressure > TOLERANCE || diffInPressure < TOLERANCE)){
+               throw  new DoorException("Pressure unequal");
+            }
+            state = DoorState.OPEN;
 	}
 	
 	@Override
 	public void close() throws DoorException {
-		//TODO - implement method
+	    if(state == DoorState.CLOSED){
+               throw  new DoorException("Door already closed");
+            }
+            state = DoorState.CLOSED;
 	}
 
 	@Override
 	public double getExternalPressure() {
-		//TODO - implement method
-		return 0.0;
+	    return exSensor.getPressure();
 	}
 
 	@Override
 	public double getInternalPressure() {
-		//TODO - implement method
-		return 0.0;
+	    return inSensor.getPressure();
 	}
 
 	@Override
 	public boolean isOpen() {
-		return state == DoorState.OPEN;
+            return state == DoorState.OPEN;
 	}
 
 	@Override
 	public boolean isClosed() {
-		return state == DoorState.CLOSED;
+	    return state == DoorState.CLOSED;
 	}
 
 	public String toString() {
-		return String.format(
-			"Door: state: %s, external pressure: %3.1f bar, internal pressure: %3.1f bar", 
-			state, exSensor.getPressure(), inSensor.getPressure());
+	    return String.format(
+	        "Door: state: %s, external pressure: %3.1f bar, internal pressure: %3.1f bar", 
+		state, exSensor.getPressure(), inSensor.getPressure());
 	}	
 
 }
---
+++ test/DoorTest.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author angry
+ */
+public class DoorTest {
+    
+    public DoorTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    
+    @Test
+    public void doorConstructorTest(){
+     
+    }
+    @Test
+    public void doorOpenTest(){
+    
+    }
+    @Test
+    public void doorCloseTest(){
+      
+    }
+    @Test
+    public void doorGetPressureTest(){
+      
+    }
+    @Test
+    public void doorStateTest(){
+     
+    }
+    
+}
