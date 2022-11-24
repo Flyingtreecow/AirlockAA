08f275d91efae08e25d86d3916ad97744cf0d1d3
Flyingtreecow <angrybirdnealon@gmail.com>
2022-11-24 13:16:28 +1100

    initiallised Airlock.java

--- src/airlock/entities/AirLock.java
+++ src/airlock/entities/AirLock.java
@@ -15,57 +15,141 @@ public class AirLock implements IAirLock{
 	
 
 	public AirLock(IDoor externalDoor, IDoor internalDoor, IPressureSensor lockSensor) {
-		//TODO - implement method
+            outerDoor = externalDoor;
+            innerDoor = internalDoor;
+            this.lockSensor = lockSensor;
+            mode = OperationMode.MANUAL;
+            if (innerDoor.isClosed()&&outerDoor.isClosed()){
+                state = AirLockState.SEALED;
+            } 
+            else {
+                state = AirLockState.UNSEALED;
+            
+            }
 	}
 		
 	@Override
-	public void openOuterDoor() throws AirLockException {
-		//TODO - implement method
+	public void openOuterDoor() throws AirLockException  {
+	    if (outerDoor.isOpen()){
+                throw new AirLockException("Door is already open");
+            }
+            else{
+               try{    
+                  if (mode == OperationMode.AUTO){
+                      if(innerDoor.isOpen()){
+                         innerDoor.close();
+                      }
+                      lockSensor.setPressure(outerDoor.getExternalPressure());
+                  }
+                  outerDoor.open();
+                  state = AirLockState.UNSEALED;
+                }
+               catch(DoorException e){
+                  throw new AirLockException(new DoorException("Door malfunction"));
+               }
+               catch(PressureException e){
+                  throw new AirLockException(new PressureException("Pressure malfunction"));
+               }
+            }
 	}
 		
 	@Override
 	public void closeOuterDoor() throws AirLockException {
-		//TODO - implement method
+            try{
+               outerDoor.close();
+               if (innerDoor.isClosed()){
+                   state = AirLockState.SEALED;
+               }
+            }
+            catch(DoorException e){
+               throw new AirLockException(new DoorException("Door malfunction"));
+            }
 	}
 	
 	@Override
 	public void openInnerDoor() throws AirLockException {
-		//TODO - implement method
+	    if (innerDoor.isOpen()){
+                throw new AirLockException("Door is already open");
+            }
+            else{
+               try{    
+                  if (mode == OperationMode.AUTO){
+                      if(outerDoor.isOpen()){
+                         outerDoor.close();
+                      }
+                      lockSensor.setPressure(innerDoor.getInternalPressure());
+                  }
+                  innerDoor.open();
+                  state = AirLockState.UNSEALED;
+               }
+               catch(DoorException e){
+                  throw new AirLockException(new DoorException("Door malfunction"));
+               }
+               catch(PressureException e){
+                  throw new AirLockException(new PressureException("Pressure malfunction"));
+               }
+            }
 	}
 	
 	@Override
 	public void closeInnerDoor() throws AirLockException {
-		//TODO - implement method
+	    try{
+               innerDoor.close();
+               if (outerDoor.isClosed()){
+                   state = AirLockState.SEALED;
+               }
+            }
+            catch(DoorException e){
+               throw new AirLockException(new DoorException("Door malfunction"));
+            }
 	}
 	
 	@Override
 	public void equaliseWithCabinPressure() throws AirLockException {
-		//TODO - implement method
+	    if ( state == AirLockState.UNSEALED){
+                throw new AirLockException("Airlock is currently unsealed");
+            }
+            else {
+                try {
+                    lockSensor.setPressure(innerDoor.getInternalPressure());
+                } 
+                catch (PressureException e) {
+                    throw new AirLockException(new PressureException("Pressure sensor malfunction"));
+                }
+             
+            }
 	}
 
 	@Override
 	public void equaliseWithEnvironmentPressure()  throws AirLockException {
-		//TODO - implement method
+		  if ( state == AirLockState.UNSEALED){
+                throw new AirLockException("Airlock is currently unsealed");
+            }
+            else {
+               try {
+                    lockSensor.setPressure(outerDoor.getExternalPressure());
+                } 
+                catch (PressureException e) {
+                    throw new AirLockException(new PressureException("Pressure sensor malfunction"));
+                }
+            }
 	}
 
 	@Override
 	public void toggleOperationMode() throws AirLockException{
-		//TODO - implement method
+            if ( state == AirLockState.UNSEALED){
+                throw new AirLockException("Airlock is currently unsealed");
+            }
+            else{
+               if (state == AirLockState.UNSEALED){
+                   state = AirLockState.SEALED;
+               }
+               if (state == AirLockState.SEALED){
+                   state = AirLockState.UNSEALED;
+               }
+            }
 	}
-	
-	@Override
-	public boolean isOuterDoorClosed() {
-		//TODO - implement method
-		return false;
-	}
-
-	@Override
-	public boolean isInnerDoorClosed() {
-		//TODO - implement method
-		return false;
-	}
-
-	@Override
+        @Override
 	public boolean isInManualMode() {
 		return mode == OperationMode.MANUAL;
 	}
@@ -74,6 +158,18 @@ public class AirLock implements IAirLock{
 	public boolean isInAutoMode() {
 		return mode == OperationMode.AUTO;
 	}
+	
+	@Override
+	public boolean isOuterDoorClosed() {
+            return outerDoor.isClosed();
+	}
+
+	@Override
+	public boolean isInnerDoorClosed() {
+	    return innerDoor.isClosed();
+	}
+
+	
 
 	@Override
 	public boolean isSealed() {
